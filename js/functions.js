00ce856b0e86a2b48f1ca4ac0b820861ed0b2b3e
anatoly <anatolijdegtyar@yandex.ru>
2024-03-09 22:52:47 +0300

    Добавляет функцию для проверки длительности рабочих встреч

--- js/functions.js
+++ js/functions.js
@@ -44,20 +44,12 @@ const getNumbers = (string = '1') => {
 
 getNumbers('1 кефир, 0.5 батона');
 
-/*
-'8:00' - начало рабочего дня
-'17:30' - конец рабочего дня
-'14:00' - начало встречи
-90 - продолжительность встречи в минутах
+const getDataToArray = (toArray) => {
+  const result = toArray.split(':');
+  return result;
+};
 
-имяФункции('08:00', '17:30', '14:00', 90); // true
-имяФункции('8:0', '10:0', '8:0', 120);     // true
-имяФункции('08:00', '14:30', '14:00', 90); // false
-имяФункции('14:00', '17:30', '08:0', 90);  // false
-имяФункции('8:00', '17:30', '08:00', 900); // false
-*/
-
-const getTimeMeetings = (startWorkTime = '09:00', endWorkTime = '18:00', startMeeting = '08:10', meetingsTime = '110') => {
+const getTimeMeetings = (startWorkTime = '8:00', endWorkTime = '17:30', startMeeting = '08:00', meetingTime = '10') => {
 
   const HOUR_IN_MINUTE = 60;
 
@@ -67,22 +59,25 @@ const getTimeMeetings = (startWorkTime = '09:00', endWorkTime = '18:00', startMe
   const meetingDurationToArray = getDataToArray(startMeeting);
 
   const getMeetingDuration = () => {
-    const meetingsTimeHours = Math.floor(meetingsTime / HOUR_IN_MINUTE);
-    const meetingsTimeMinutes = meetingsTime % HOUR_IN_MINUTE;
 
-    const a = Number(meetingDurationToArray[0]) + meetingsTimeHours;
-    const b = Number(meetingDurationToArray[1]) + meetingsTimeMinutes;
-    console.log(b);
+    const meetingTimeHour = Math.floor(meetingTime / HOUR_IN_MINUTE);
+    const meetingTimeMinutes = meetingTime % HOUR_IN_MINUTE;
+
+    const interimResultHour = Number(meetingDurationToArray[0]) + meetingTimeHour;
+    const interimResultMinute = Number(meetingDurationToArray[1]) + ((Math.floor(meetingTimeMinutes / HOUR_IN_MINUTE)) + (Math.floor(meetingTimeMinutes % HOUR_IN_MINUTE)));
+
+    const minuteMeetingInHour = Math.floor(interimResultMinute / HOUR_IN_MINUTE);
+
+    const minuteMeeting = Math.floor(interimResultMinute % HOUR_IN_MINUTE);
+    const hourMeeting = interimResultHour + minuteMeetingInHour;
+
+    const stringMeetingDuration = String(hourMeeting) + String(minuteMeeting);
+
+    return stringMeetingDuration;
   };
 
-  getMeetingDuration();
+  const finalMeetingDuration = Number(getMeetingDuration());
+  return startMeetingNumber >= startWorkTimeNumber && startMeetingNumber < endWorkTimeNumber && finalMeetingDuration <= endWorkTimeNumber;
 };
 
-const getDataToArray = (toArray) => {
-  return toArray.split(':');
-};
-
-//getTimeMeetings();
-
-const result = getTimeMeetings();
-console.log(result);
+getTimeMeetings();
