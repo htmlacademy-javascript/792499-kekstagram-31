b913014fa81af1a8ec1984ec66dfe61980057dda
Keks <keks@htmlacademy.ru>
2024-03-30 17:22:55 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,8 @@
+import { createThumbnails } from './create-thumbnails.js';
+import { createBigPhoto } from './create-big-photo.js';
+import { getUploadModal } from './upload-photo.js';
+//import { getInputRange } from './slider.js';
+createThumbnails();
+createBigPhoto();
+getUploadModal();
+//getInputRange();
